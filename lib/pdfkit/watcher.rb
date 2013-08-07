9128c52d636ddbe0d0dff9460e00f9be625ee02e
Gabriel Dehan <dehan.gabriel@gmail.com>
2013-08-07 15:43:24 +0200

    Added a 5 minute time out to each pdf generation

--- lib/pdfkit/watcher.rb
+++ lib/pdfkit/watcher.rb
@@ -1,11 +1,19 @@
 class PDFKit
+  class PDFGenerationError < StandardError
+    def initialize(msg)
+      super("Generation failed: #{msg}")
+    end
+  end
+
   class Watcher
     def initialize(file, opts)
       @file = file
       @delay = opts[:with_delay] || 0
+      @timeout = opts[:timeout] || 300
     end
 
     def watch_for(pattern, &block)
+      start = Time.now
       sleep @delay unless @delay.zero?
 
       f = File.open(@file, "r")
@@ -16,6 +24,12 @@ class PDFKit
         if read_as_utf8(line) =~ pattern
           block.call(read_as_utf8(line), pattern)
           break
+        else
+          if Time.now > start + @timeout
+            block.call(read_as_utf8(line), pattern)
+            raise PDFGenerationError.new("generation timed out")
+            break
+          end
         end
       end
       f.close
